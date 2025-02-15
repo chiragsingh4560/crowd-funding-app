748d639d8ad7838de2a9bde4585fddab5238e51e
DJSCE Student <DJSCE.Student@SVKMGRP.COM>
2025-02-15 12:43:03 +0530

    Updated handleSubmit function with alert

--- crowdFundingApp/src/CrowdFundingApp.jsx
+++ crowdFundingApp/src/CrowdFundingApp.jsx
@@ -1,18 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FundingCard from "./FundingCard";
 
 const CrowdFundingApp = () => {
   const goalArr = [1000, 500, 2000, 1500];
   const [amtRaised, SetAmtRaised] = useState(new Array(goalArr.length).fill(0));
-  const [inputVal, SetAmtVal] = useState(new Array(goalArr.length).fill(""));
+  const [inputVal, SetInputVal] = useState(new Array(goalArr.length).fill(""));
+
   const handleChange = (e, projectNo) => {
-    console.log(
-      `input changed on project ${projectNo} input is: ${e.target.value}`
-    );
+    SetInputVal((prev) => {
+      const updatedInputs = [...prev];
+      updatedInputs[projectNo - 1] = e.target.value;
+      return updatedInputs;
+    });
   };
+
+  const clearInputs = () => {
+    SetInputVal(new Array(goalArr.length).fill(""));
+  };
+
   const handleSubmit = (e, projectNo) => {
     e.preventDefault();
-    console.log(`submit clicked on project ${projectNo}`);
+    const amtNeeded = goalArr[projectNo - 1] - amtRaised[projectNo - 1];
+    const enteredAmount = Number(inputVal[projectNo - 1]);
+    if (enteredAmount > amtNeeded) {
+      alert(`Amount exceeds needed funds! You only need ₹${amtNeeded}.`);
+      return;
+    } else {
+      SetAmtRaised((prev) => {
+        const updatedRaised = [...prev];
+        updatedRaised[projectNo - 1] = enteredAmount + amtRaised[projectNo - 1];
+        return updatedRaised;
+      });
+    }
+    clearInputs();
   };
 
   return (
